50f3157c36fd674b5a286a1a593df713b810d5b1
AvelinoC5 <avelino_63@hotmail.com>
2022-12-16 18:33:01 -0500

    update file

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ HBNB_API_HOST = getenv("HBNB_API_HOST")
 HBNB_API_PORT = getenv("HBNB_API_PORT")
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+#CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
